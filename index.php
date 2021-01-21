ba63312a38acee5a9491e0010c20d841562f5d8c
maxbehzadi82 <69126271+maxbehzadi82@users.noreply.github.com>
2021-01-21 10:47:29 +0100

    input field added

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,16 +8,19 @@
     <link rel="stylesheet" href="style.css">
 
 </head>
+
 <body>
     <div class="container">
-        <form action="" method="post">
-        <div>
-        </div>
-        </form
-        
+        <form action="welcome.php" method="post" enctype="multipart/form-data">
+            <div class="row">
+                <input type="text" name="username" id="username" placeholder="Username">
+            </div>
+            <div class="row">
+                <input type="email" name="email" id="email"  placeholder="Email">
+            </div>
+             <button class="btn btn-primary">Login</button>
         </form>
-    
     </div>
-
 </body>
-</html>
+
+</html> 
